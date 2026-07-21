1f5263b2319bbc2a5c13d732685b6de9733db93a
MatteaE <enrico.mattea@unifr.ch>
2026-07-21 11:23:33 +0200

    Improved console output on Windows

--- functions/func_customlog.R
+++ functions/func_customlog.R
@@ -8,6 +8,8 @@
 #                 the default sink() behavior which does not capture the output of message().     #
 ################################################################################################### 
 
+# Note: this function does not automatically add a whitespace between multiple arguments.
+
 # 0 or "b(asic)" calls message() with no formatting
 # 1 or "w(arning)" calls message() prepending a bold, yellow-shaded "WARNING" to the text
 # 2 or "f(atal)" calls message() prepending a bold, red-shaded "FATAL: " to the text
@@ -15,6 +17,7 @@
 # 4 or "i(nfo)" calls message() with bold text
 func_customlog <- function(..., level = c("basic", "warning", "fatal", "success", "info")) {
   
+  # Find out which level is the log (either numeric or text).
   if (is.character(level)) {
     level <- match.arg(level)
   } else {
@@ -32,36 +35,57 @@ func_customlog <- function(..., level = c("basic", "warning", "fatal", "success"
   }
   
   
+  # Concatenate input into a single character.
   txt <- paste(lapply(list(...), as.character), collapse = "")
+  
+  # Select OS-specific function to write to console.
+  # RStudio on Windows uses an ugly red text for message(),
+  # so we use cat() and ANSI bold text instead.
+  if (Sys.info()["sysname"] == "Windows") {
+    
+    func_consolewrite <- function(prefix, txt) {
+      cat(paste0(prefix,
+                 "\033[1m", txt, "\033[0m", "\n"))
+    }
+    
+    # On other systems use message(): RStudio uses a nice grey background.
+  } else {
+    
+    func_consolewrite <- function(prefix, txt) {
+      message(paste0(prefix,
+                     txt))
+    }
+  }
   
   
   # Temporarily suspend the split sink(), since we need to write to the log file and not to the console.
   flush(logcon)
   sink()
   
+  # Write the message to console and log file.
   if (level == "basic") {
     
-    message(...)
+    func_consolewrite("", txt)
     writeLines(paste0(txt, "\n"), con = logcon, sep = "")
     
   } else if (level == "warning") {
     
-    message("\033[1;48;5;226;38;5;16m WARNING \033[0m " , ...) # WARNING in bold black text on yellow background
+    func_consolewrite("\033[1;48;5;226;38;5;16m WARNING \033[0m ", txt) # WARNING in bold black text on yellow background
     writeLines(paste0("* WARNING: ", txt, "\n"), con = logcon, sep = "")
     
   } else if (level == "fatal") {
     
-    message("\033[1;48;5;196;38;5;231m FATAL \033[0m ", ...) # FATAL in bold white text on red background
+    func_consolewrite("\033[1;48;5;196;38;5;231m FATAL \033[0m ", txt) # FATAL in bold white text on red background
     writeLines(paste0("** FATAL: ", txt, "\n"), con = logcon, sep = "")
     
   } else if (level == "success") {
     
-    message("\033[1;48;5;28;38;5;231m SUCCESS \033[0m ", ...) # SUCCESS in bold white text on green background
+    func_consolewrite("\033[1;48;5;28;38;5;231m SUCCESS \033[0m ", txt) # SUCCESS in bold white text on green background
     writeLines(paste0("** SUCCESS: ", txt, "\n"), con = logcon, sep = "")
     
   } else {
     
-    message("\033[1;48;5;45;38;5;16m INFO \033[0m ", ...) # INFO in bold black text on light blue background
+    func_consolewrite("\033[1;48;5;45;38;5;16m INFO \033[0m ", txt) # INFO in bold black text on light blue background
     writeLines(paste0("INFO: ", txt, "\n"), con = logcon, sep = "")
     
   }
