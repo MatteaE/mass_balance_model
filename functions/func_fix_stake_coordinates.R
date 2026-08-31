038e664b7e1c97d788c57106a28e385b08495001
MatteaE <enrico.mattea@unifr.ch>
2026-08-31 12:54:05 +0200

    Made more robust the recovery mechanism for stakes with wrong CRS

--- functions/func_fix_stake_coordinates.R
+++ functions/func_fix_stake_coordinates.R
@@ -21,17 +21,34 @@ func_fix_stake_coordinates <- function(stake_id,
   
   point_cur <- st_point(stake_xy)
   
+  crs_success_logi <- rep(FALSE, length(crs_try))
+  coords_transf_l  <- list()
+  
   for (crs_id in 1:length(crs_try)) {
-    point_cur_sfc <- st_sfc(point_cur, crs = crs_try[crs_id])
-    point_cur_transf <- st_transform(point_cur_sfc, crs_target)
-    point_transf_coords <- as.numeric(st_coordinates(point_cur_transf))
+    point_cur_sfc             <- st_sfc(point_cur, crs = crs_try[crs_id])
+    point_cur_transf          <- st_transform(point_cur_sfc, crs_target)
+    point_transf_coords       <- as.numeric(st_coordinates(point_cur_transf))
+    coords_transf_l[[crs_id]] <- point_transf_coords
     if (all(!is.na(point_transf_coords))) {
       if ((dhm_extent[1] <= point_transf_coords[1]) && (dhm_extent[2] >= point_transf_coords[1]) && (dhm_extent[3] <= point_transf_coords[2]) && (dhm_extent[4] >= point_transf_coords[2])) {
-        func_customlog("            Point id ", stake_id, ": recovered by using the EPSG:", crs_try[crs_id], " coordinate system.", level = 0)
-        return(point_transf_coords)
+        func_customlog("            Point id ", stake_id, " might be using the EPSG:", crs_try[crs_id], " coordinate system.", level = 0)
+        crs_success_logi[crs_id] <- TRUE
       }
     }
+  } # End loop on attempted CRS
+  
+  # If a single CRS works for the current point, apply it.
+  # If more than one matches (unlikely but possible on very large areas), stop and complain.
+  # If none matches, return NA which signals that it is not possible to rescue the point.
+  crs_ok_ids <- which(crs_success_logi)
+  if (length(crs_ok_ids) == 1) {
+    return(coords_transf_l[[crs_ok_ids]])
+  } else if (length(crs_ok_ids) > 1) {
+    func_customlog("            Point id ", stake_id, " has an ambiguous coordinate system. Please check and fix its coordinates manually.", level = 2)
+    func_customlog("            Provided values: ", stake_xy[1], ", ", stake_xy[2], level = 0)
+    func_stop()
+  } else {
+    return(c(NA, NA))
   }
   
-  return(c(NA, NA))
 }
